f1ff649a991d63076361fb808fe81bcba3501ebd
Maggie Xian <111546244+meitongxian@users.noreply.github.com>
2026-03-17 12:37:30 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+let now = new Date();
+
+let yearNumber = now.getFullYear();
+
+let isLeapYear = leapYear(yearNumber);
+
+function leapYear(year) {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+let dayTotal = 365;
+
+if (isLeapYear == true) {
+    dayTotal = 366;
+}
+
+let numbersWrapper = document.querySelector(".numbers-wrapper");
+ 
+placeNumbers();
+ 
+function placeNumbers() {
+    numbersWrapper.innerHTML = "";
+ 
+    const radiusX = window.innerWidth / 2 * 0.9;
+    const radiusY = window.innerHeight / 2 * 0.9;
+ 
+    for (let i = 1; i <= dayTotal; i++) {
+        let angle = (i * (Math.PI * 2 / dayTotal) - Math.PI / 2) - (Math.PI / dayTotal);
+        let x = window.innerWidth / 2 + Math.cos(angle) * radiusX;
+        let y = window.innerHeight / 2 + Math.sin(angle) * radiusY;
+        const dayNumber = document.createElement("span");
+        dayNumber.textContent = i;
+        dayNumber.style.left = `${x}px`;
+        dayNumber.style.top = `${y}px`;
+
+        let currentDay = getDate();
+
+        if (i == currentDay) {
+            dayNumber.className = "number number-selected mono";
+        } else {
+            dayNumber.className = "number mono";
+        }
+
+        numbersWrapper.appendChild(dayNumber);
+    }
+}
+ 
+window.addEventListener('resize', placeNumbers);
+
+function getDate() {
+    var start = new Date(now.getFullYear(), 0, 0);
+    var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+    var oneDay = 1000 * 60 * 60 * 24;
+    var day = Math.floor(diff / oneDay);
+    return day;
+}
